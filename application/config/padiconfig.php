b5e414a02084e95503639b9e5d509634f5428888
pujie <puji@padi.net.id>
2023-02-02 14:00:28 +0700

    add config to determine deve or prod state

--- application/config/padiconfig.php
+++ application/config/padiconfig.php
@@ -2,3 +2,4 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 $config['uploadpath'] = 'C:\phpworkspace\hotspot\uploads\ ';
 $config['uploadpathserver'] = '/home/klien/www/hotspot/uploads/ ';
+$config['development'] = true;
